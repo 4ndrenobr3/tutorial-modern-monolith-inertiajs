a2d84abb38af565af174b297f7e27f72b6c07d7a
Andre Nobre <nobre4ndr3@gmail.com>
2021-11-27 15:38:36 -0300

    Aula 08 Active Links

---
+++ resources/js/Shared/NavLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <Link
+    href="/"
+    :class="{ 'font-bold underline': active }"
+  >
+    <slot />
+  </Link>
+</template>
+
+<script>
+import { Link } from '@inertiajs/inertia-vue3'
+
+export default {
+    components: { Link },
+    props: {
+        active: Boolean
+    }
+};
+</script>
+
+<style>
+</style>
